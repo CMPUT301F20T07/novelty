93950250bda3bbfb07860d426c51e4901c8f1b32
Eleshba <eleshba@ualberta.ca>
2020-11-03 22:43:50 -0700

    updated json and database.java

--- Novelty/app/src/main/java/com/example/novelty/Database.java
+++ Novelty/app/src/main/java/com/example/novelty/Database.java
@@ -1,5 +1,23 @@
 package com.example.novelty;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class Database {
-    String useless;
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public static FirebaseStorage storage = FirebaseStorage.getInstance();
+    public static StorageReference storageRef = storage.getReference();
+
+    public static DocumentReference getUserProfileRef(String userID) {
+        return Database.db.collection(userID)
+                .document("userProfile");
+    }
+
+    public static DocumentReference getUserBooksRef(String userID) {
+        return Database.db.collection(userID)
+                .document("books");
+    }
 }
