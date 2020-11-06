31bec03834e096f5a9f4a75df020bb332ae3c867
Hao Gu <gu2@ualberta.ca>
2020-11-06 20:24:57 +0800

    make some modification

--- Novelty/app/src/main/java/com/example/novelty/activity/Register.java
+++ Novelty/app/src/main/java/com/example/novelty/activity/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,17 +16,27 @@ import android.widget.Toast;
 
 import com.example.novelty.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
-
-    EditText myUsername, myEmail, myPassword, myPhone;
-    Button myRegisterBtn;
-    TextView myLoginLink; // used to direct user to login activity
-    FirebaseAuth fAuth; //provided by firebase used to register the user.
-    ProgressBar progressBar;
+    public static final String TAG = "TAG";
+    private EditText myUsername, myEmail, myPassword, myPhone;
+    private Button myRegisterBtn;
+    private TextView myLoginLink; // used to direct user to login activity
+    private FirebaseAuth fAuth; //provided by firebase used to register the user.
+    private ProgressBar progressBar;
+    private FirebaseFirestore fStore;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +51,7 @@ public class Register extends AppCompatActivity {
 
         myRegisterBtn = findViewById(R.id.register_button);
         myLoginLink = findViewById(R.id.login_link);
-
+        fStore = FirebaseFirestore.getInstance();
         fAuth = FirebaseAuth.getInstance(); // gets current instance of database from firebase to perform operations
         progressBar = findViewById(R.id.progressBar);
 
@@ -51,23 +62,24 @@ public class Register extends AppCompatActivity {
         }
 
         //click the register button
-
         myRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //get the values of email and password fields
-                String email = myEmail.getText().toString().trim(); // trim() eliminates leading and trailing spaces.
+                //get the values of all fields
+                final String email = myEmail.getText().toString().trim(); // trim() eliminates leading and trailing spaces.
                 String password = myPassword.getText().toString().trim();
+                final String username = myUsername.getText().toString();
+                final String phone = myPhone.getText().toString();
 
-                //validate the email and password
-                //check if there was something entered for email
+
+                //validate the email field not blank
                 if (TextUtils.isEmpty(email)){
                     //display error message
                     myEmail.setError("Email is required");
                     return ;
                 }
 
-                //check if there was something entered for password
+                // validate password field not blank
                 if (TextUtils.isEmpty(password)){
                     //display error message
                     myPassword.setError("Password is required");
@@ -88,16 +100,37 @@ public class Register extends AppCompatActivity {
                         //if task is successful then successfully created a user
                         //if not complete
                         if (task.isSuccessful()){
+
                             //small popup to show success
                             Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+
+                            //add data to FireStore
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference userDoc = Database.getUserRef(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("username",username);
+                            user.put("email",email);
+                            user.put("phone",phone);
+
+                           userDoc.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: userProfile is created for "+ userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
 
                             //send user to main activity
                             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                             startActivity(intent);
 
                         }else
-                            Toast.makeText(Register.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.INVISIBLE); // on Error, Hide spinner
+                            Toast.makeText(Register.this, "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.INVISIBLE); // on Error, Hide spinner
                     }
                 }) ;
             }
