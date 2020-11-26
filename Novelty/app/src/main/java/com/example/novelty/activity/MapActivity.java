1967cb29ee84eec8defb1ab8e17b2e6fa112a663
hexingjie329A <xingjie1@ualberta.ca>
2020-11-26 11:48:07 -0700

    bew branch location

--- Novelty/app/src/main/java/com/example/novelty/activity/MapActivity.java
+++ Novelty/app/src/main/java/com/example/novelty/activity/MapActivity.java
@@ -19,6 +19,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_map);
 
         MapView mMap = (MapView) findViewById(R.id.map);
