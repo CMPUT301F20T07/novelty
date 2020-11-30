83c8ee1baffd9109c4014d54ac06de72f4e43795
Hao Gu <gu2@ualberta.ca>
2020-12-01 05:49:42 +0800

    Merge Xingjie's part

--- Novelty/app/src/main/java/com/example/novelty/activity/Database.java
+++ Novelty/app/src/main/java/com/example/novelty/activity/Database.java
@@ -14,28 +14,43 @@ public class Database {
 //    public static StorageReference storageRef = storage.getReference();
 
     /**
+     * gets user
+     *
+     * @return  user
+     */
+    public static CollectionReference getUserRef() {
+        return Database.db.collection("users");
+    }
+
+    /**
      * gets reference
+     *
      * @param userID unique userID for user from firebase auth.
      * @return document reference for the user
      */
     public static DocumentReference getUserRef(String userID) {
         return Database.db.collection("users")
-                .document(userID);
+                  .document(userID);
     }
 
-    public static DocumentReference getBookInfo(String ISBN) {
-        return Database.db.collection("books")
-                .document(ISBN);
+    public static DocumentReference addBookInfo(String userID, String ISBN) {
+        return Database.db.collection("users")
+                  .document(userID)
+                  .collection("avail").document(ISBN);
     }
-    public static void addBookInfo(HashMap book, String ISBN) {
-        Database.db.collection("books").document(ISBN).set(book);
+
+    public static CollectionReference getBookInfo(String userID) {
+        return Database.db.collection("users")
+                  .document(userID)
+                  .collection("available");
     }
 
     /**
      * gets a book's reference
+     *
      * @param collec collection reference for the books
-     * @param ISBN unique ISBN of the book to access document
-     * @return document reference for the specified book
+     * @param ISBN   unique ISBN of the book to access document
+     * @return document reference for the s  pecified book
      */
     public static DocumentReference getBookRef(CollectionReference collec, String ISBN) {
         return collec.document(ISBN);
@@ -43,62 +58,68 @@ public class Database {
 
     /**
      * The books that I have available
+     *
      * @param userID string unique userID from firebase auth.
      * @return collection reference to the "available"
      */
     public static CollectionReference userAvailRef(String userID) {
         return Database.db.collection("users")
-                .document(userID).collection("available");
+                  .document(userID).collection("available");
     }
 
     /**
      * The books that I'm borrowed
+     *
      * @param userID string unique userID from firebase auth.
      * @return collection reference to the "borrowed"
      */
     public static CollectionReference userBorrowedRef(String userID) {
         return Database.db.collection("users")
-                .document(userID).collection("borrowed");
+                  .document(userID).collection("borrowed");
     }
 
     /**
      * as an owner, the requests that I received
+     *
      * @param userID string unique userID from firebase auth.
      * @return collection reference to the "received requests"
      */
     public static CollectionReference userReceivedRequestRef(String userID) {
         return Database.db.collection("users")
-                .document(userID).collection("received requests");
+                  .document(userID).collection("received requests");
     }
 
     /**
      * as a borrower, the requests that I made
+     *
      * @param userID string unique userID from firebase auth.
      * @return collection reference to the "my requets"
      */
     public static CollectionReference userRequestRef(String userID) {
         return Database.db.collection("users")
-                .document(userID).collection("my requests");
+                  .document(userID).collection("my requests");
     }
 
     /**
      * as an owner, the requests that I accepted
+     *
      * @param userID string unique userID from firebase auth.
      * @return collection reference to the "accepted requests"
      */
     public static CollectionReference userAcceptedRef(String userID) {
         return Database.db.collection("users")
-                .document(userID).collection("accepted requests");
+                  .document(userID).collection("accepted requests");
     }
 
     /**
      * as a borrower, my requests that got accepted
+     *
      * @param userID string unique userID from firebase auth.
      * @return collection reference to "my accepted requests"
      */
     public static CollectionReference userRequestAcceptedRef(String userID) {
         return Database.db.collection("users")
-                .document(userID).collection("my accepted requests");
+                  .document(userID).collection("my accepted requests");
     }
 
 }
